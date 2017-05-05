62bc41a9d4568b1b475551d515c26c921b1683ed
Gabriel Gauci Maistre <gabegm@Gabriels-MacBook-Pro.local>
2017-05-05 19:00:49 +0200

    the voting classifier too

--- mlearning/Votingc.py
+++ mlearning/Votingc.py
@@ -1,5 +1,6 @@
 import functions as fc
 import random as rand
+from collections import OrderedDict
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier, VotingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -8,56 +9,73 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import GaussianNB, BernoulliNB
 from sklearn.svm import SVC
 
-df = fc.get_time_series('AAPL')
 
-# add the outcome variable, 1 if the trading session was positive (close>open), 0 otherwise
-df['outcome'] = df.apply(lambda x: 1 if x['adj_close'] > x['adj_open'] else -1, axis=1)
+def run(tickers='AAPL', start=None, end=None, n_steps=21):
+    data = OrderedDict()
+    pred_data = OrderedDict()
+    forecast_data = OrderedDict()
 
-df = fc.get_sma_classifier_features(df)
+    for ticker in tickers:
+        data[ticker] = fc.get_time_series(ticker, start, end)
 
-# cross-validation testing
-split = rand.uniform(0.60, 0.80)
+        # add the outcome variable, 1 if the trading session was positive (close>open), 0 otherwise
+        data[ticker]['outcome'] = data[ticker].apply(lambda x: 1 if x['adj_close'] > x['adj_open'] else -1, axis=1)
 
-train_size = int(len(df) * split)
+        data[ticker] = fc.get_sma_classifier_features(data[ticker])
 
-train, test = df[0:train_size], df[train_size:len(df)]
+        # cross-validation testing
+        split = rand.uniform(0.60, 0.80)
 
-features = ['sma_2', 'sma_3', 'sma_4', 'sma_5', 'sma_6']
+        train_size = int(len(data[ticker]) * split)
 
-# values of features
-X = list(train[features].values)
+        train, test = data[ticker][0:train_size], data[ticker][train_size:len(data[ticker])]
 
-# target values
-Y = list(train['outcome'])
+        features = ['sma_2', 'sma_3', 'sma_4', 'sma_5', 'sma_6']
 
-clf1 = AdaBoostClassifier()
-clf2 = RandomForestClassifier()
-clf3 = DecisionTreeClassifier()
-clf4 = KNeighborsClassifier()
-clf5 = LogisticRegression()
-clf6 = SGDClassifier()
-clf7 = MLPClassifier()
-clf8 = GaussianNB()
-clf9 = BernoulliNB()
-clf10 = SVC()
+        # values of features
+        X = list(train[features].values)
 
-mdl = VotingClassifier(estimators=[('bt', clf1),
-                                   ('rf', clf2),
-                                   ('dt', clf3),
-                                   ('knn', clf4),
-                                   ('lgt', clf5),
-                                   ('sgd', clf6),
-                                   ('mlp', clf7),
-                                   ('gnb', clf8),
-                                   ('bnb', clf9),
-                                   ('svm', clf10)],
-                       voting='hard').fit(X, Y)
+        # target values
+        Y = list(train['outcome'])
 
-confidence = mdl.score(test[features].values, test['outcome'].values)
+        clf1 = AdaBoostClassifier()
+        clf2 = RandomForestClassifier()
+        clf3 = DecisionTreeClassifier()
+        clf4 = KNeighborsClassifier()
+        clf5 = LogisticRegression()
+        clf6 = SGDClassifier()
+        clf7 = MLPClassifier()
+        clf8 = GaussianNB()
+        clf9 = BernoulliNB()
+        clf10 = SVC()
 
-pred = mdl.predict(test[features].values)
+        mdl = VotingClassifier(estimators=[('bt', clf1),
+                                           ('rf', clf2),
+                                           ('dt', clf3),
+                                           ('knn', clf4),
+                                           ('lgt', clf5),
+                                           ('sgd', clf6),
+                                           ('mlp', clf7),
+                                           ('gnb', clf8),
+                                           ('bnb', clf9),
+                                           ('svm', clf10)],
+                               voting='hard').fit(X, Y)
 
-# out-of-sample test
-n_steps = 21
+        print(mdl)
 
-forecast = fc.forecast_classifier(model=mdl, sample=test, features=features, steps=n_steps)['outcome']
+        confidence = mdl.score(test[features].values, test['outcome'].values)
+
+        print("{} Voting Classifier\n"
+              "-------------\n"
+              "Confidence: {}\n".format(ticker,
+                                        confidence))
+
+        pred = mdl.predict(test[features].values)
+
+        pred_data[ticker] = pred
+
+        # out-of-sample test
+        forecast_data[ticker] = fc.forecast_classifier(model=mdl, sample=test, features=features, steps=n_steps)[
+            'outcome']
+
+    return forecast_data
