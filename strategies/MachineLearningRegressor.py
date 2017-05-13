cd2cc2f427ead11c649eb3563c0f7614d3035769
Gabriel Gauci Maistre <gabegm@Gabriels-MacBook-Pro.local>
2017-05-13 08:44:24 +0200

    cleaned up some code

--- strategies/MachineLearningRegressor.py
+++ strategies/MachineLearningRegressor.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy as np
 from collections import OrderedDict
 from zipline.algorithm import TradingAlgorithm
-from zipline.api import symbol, order_target_percent, record
+from zipline.api import symbol, order_target_percent, record, set_commission, set_slippage
+from zipline.finance import commission, slippage
 import talib as ta
 
 
@@ -13,13 +14,22 @@ class MachineLearningRegressor(TradingAlgorithm):
         """
         Called once at the start of the algorithm.
         """
+        set_slippage(slippage.FixedSlippage(spread=0.00))
+        set_commission(commission.PerShare(cost=0, min_trade_cost=0))
+
+        context.invested = False
+
+        context.securities = tickers
+
         context.window_length = 50  # Amount of prior bars to study
 
-        context.data_points = 100
+        context.data_points = 100  # There needs to be enough data points to make a good model
 
         context.forecast_steps = 100  # Number of days to forecast
 
         context.trading_freq = 50  # trading frequency, days
+
+        context.forecast_difference = 15
 
         context.forecast = []
 
@@ -31,38 +41,42 @@ class MachineLearningRegressor(TradingAlgorithm):
         for ticker in tickers:
             context.recent_prices[ticker] = []
 
-        context.sma15 = []
-        context.sma50 = []
+        context.sma15 = context.sma50 = []
 
         context.X = []  # Independent, or input variables
         context.Y = []  # Dependent, or output variable
 
-        context.pred = 0  # Stores most recent prediction
+        context.pred = []  # Stores most recent prediction
+
+    def before_trading_start(context, data):
+        """
+        Called every day before market open.
+        """
 
     def handle_data(context, data):
         """
         Called every minute.
         """
-        for ticker in tickers:
-            context.recent_prices[ticker].append(data.current(symbol(ticker), 'close'))  # Update the recent prices
-            if len(context.recent_prices[ticker]) >= context.window_length + 2:  # If there's enough recent price data
+        for security in context.securities:
+            context.recent_prices[security].append(data.current(symbol(security), 'close'))  # Update the recent prices
+            if len(context.recent_prices[security]) >= context.window_length + 2:  # If there's enough recent price data
                 # Limit trading frequency
-                if len(context.recent_prices[ticker]) % context.trading_freq != 0.0:
+                if len(context.recent_prices[security]) % context.trading_freq != 0.0:
                     return
 
                 # Add independent variables, the prior changes
-                context.sma15 = get_sma(close=context.recent_prices[ticker], days=15, window=context.window_length)
-                context.sma50 = get_sma(close=context.recent_prices[ticker], days=50, window=context.window_length)
+                context.sma15 = get_sma(close=context.recent_prices[security], days=15, window=context.window_length)
+                context.sma50 = get_sma(close=context.recent_prices[security], days=50, window=context.window_length)
 
                 context.X = np.array(list(zip(context.sma15, context.sma50)))
-                context.Y = context.recent_prices[ticker]  # Add dependent variable, the final change
+                context.Y = context.recent_prices[security]  # Add dependent variable, the final change
 
-                if len(context.Y) >= context.data_points:  # There needs to be enough data points to make a good model
+                if len(context.Y) >= context.data_points:
                     context.mdl.fit(context.X, context.Y[context.window_length - 1:])  # Generate the model
 
                     context.pred = context.mdl.predict(context.X[-1:])  # Predict
 
-                    context.forecast = np.append(context.recent_prices[ticker], context.pred)
+                    context.forecast = np.append(context.recent_prices[security], context.pred)
 
                     for k in range(1, context.forecast_steps):
                         context.sma15 = get_sma(close=context.forecast, days=15, window=context.window_length)
@@ -70,22 +84,26 @@ class MachineLearningRegressor(TradingAlgorithm):
 
                         context.X = np.array(list(zip(context.sma15, context.sma50)))
 
-                        context.pred = context.mdl.predict(context.X[-1:])
+                        context.pred = np.append(context.pred, context.mdl.predict(context.X[-1:]))
 
-                        context.forecast = np.append(context.forecast, context.pred)
+                        context.forecast = np.append(context.forecast, context.pred[-1:])
 
                     # If prediction = 1, buy all shares affordable, if 0 sell all shares
-                    if (context.forecast[-1:] - context.forecast[:1]) > 50:
-                        # order(asset=symbol(ticker), amount=100)
-                        order_target_percent(asset=symbol(ticker),
-                                             target=get_percentage_difference(first=context.forecast[:1],
+                    if (context.forecast[-1:] - context.pred[0]) > context.forecast_difference:
+                        # order(asset=symbol(security), amount=100)
+                        order_target_percent(asset=symbol(security),
+                                             target=get_percentage_difference(first=context.pred[0],
                                                                               last=context.forecast[-1:]))
-                    elif (context.forecast[-1:] - context.forecast[:1]) < 50:
-                        order_target_percent(asset=symbol(ticker),
-                                             target=-get_percentage_difference(first=context.forecast[:1],
+                    elif (context.forecast[-1:] - context.pred[0]) < -context.forecast_difference:
+                        order_target_percent(asset=symbol(security),
+                                             target=-get_percentage_difference(first=context.pred[0],
                                                                                last=context.forecast[-1:]))
 
-                    record(prediction=int(context.pred))
+    def record_vars(context, data):
+        """
+        Plot variables at the end of each day.
+        """
+        record(prediction=int(context.pred[0]))
 
 
 def get_sma(close, days, window):
@@ -98,7 +116,7 @@ def get_sma(close, days, window):
 
 
 def get_percentage_difference(first, last):
-    percent = ((last - first) / first) / 0.100
+    percent = ((last - first) / first) * 10
 
     percent = float(np.around(percent, 2))
 
